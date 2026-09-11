cefb0176111a3edfbda64b08894cca2543bfc52f
Anderson Kaiti <anderkaiti@gmail.com>
2026-09-11 14:34:10 -0300

    :sparkles: feat(steps): validate fields before advancing to next step

--- src/components/steps/address-step.tsx
+++ src/components/steps/address-step.tsx
@@ -1,3 +1,9 @@
+import { ErrorMessage } from '@hookform/error-message'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AlertCircleIcon } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Alert, AlertDescription } from '../ui/alert'
 import { Field, FieldGroup, FieldLabel } from '../ui/field'
 import { Input } from '../ui/input'
 import {
@@ -7,33 +13,89 @@ import {
 } from '../ui/stepper'
 import { StepHeader } from './step-header'
 
+const addressStepSchema = z.object({
+  state: z.string().min(1, 'Informe o seu Estado'),
+  city: z.string().min(1, 'Informe a sua cidade'),
+  street: z.string().min(1, 'Informe a sua rua'),
+})
+
 export function AddressStep() {
+  const form = useForm({
+    resolver: zodResolver(addressStepSchema),
+  })
+
+  const handleSubmit = form.handleSubmit(async (formData) => {
+    console.log(formData)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  })
+
   return (
-    <div className="w-full">
+    <form className="w-full" onSubmit={handleSubmit}>
       <StepHeader title="Endereço" description="De onde você é?" />
 
       <FieldGroup>
         <Field>
           <FieldLabel htmlFor="state">Estado</FieldLabel>
-          <Input id="state" />
+
+          <Input id="state" {...form.register('state')} />
+
+          <ErrorMessage
+            errors={form.formState.errors}
+            name="state"
+            render={({ message }) => (
+              <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertDescription>{message}</AlertDescription>
+              </Alert>
+            )}
+          />
         </Field>
 
         <Field>
           <FieldLabel htmlFor="city">Cidade</FieldLabel>
-          <Input id="city" />
+
+          <Input id="city" {...form.register('city')} />
+
+          <ErrorMessage
+            errors={form.formState.errors}
+            name="city"
+            render={({ message }) => (
+              <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertDescription>{message}</AlertDescription>
+              </Alert>
+            )}
+          />
         </Field>
 
         <Field>
           <FieldLabel htmlFor="street">Endereço</FieldLabel>
-          <Input id="street" />
+
+          <Input id="street" {...form.register('street')} />
+
+          <ErrorMessage
+            errors={form.formState.errors}
+            name="street"
+            render={({ message }) => (
+              <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertDescription>{message}</AlertDescription>
+              </Alert>
+            )}
+          />
         </Field>
       </FieldGroup>
 
       <StepperFooter>
-        <StepperBackButton />
+        <StepperBackButton disabled={form.formState.isSubmitting} />
 
-        <StepperNextButton />
+        <StepperNextButton
+          type="submit"
+          onClick={handleSubmit}
+          disabled={form.formState.isSubmitting}
+        />
       </StepperFooter>
-    </div>
+    </form>
   )
 }
--- src/components/steps/personal-data-step.tsx
+++ src/components/steps/personal-data-step.tsx
@@ -1,3 +1,11 @@
+import { ErrorMessage } from '@hookform/error-message'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useStepper } from '@hooks/use-stepper'
+import { AlertCircleIcon } from 'lucide-react'
+import { Controller, useForm } from 'react-hook-form'
+import { PatternFormat } from 'react-number-format'
+import { z } from 'zod'
+import { Alert, AlertDescription } from '../ui/alert'
 import { Field, FieldGroup, FieldLabel } from '../ui/field'
 import { Input } from '../ui/input'
 import {
@@ -7,9 +15,32 @@ import {
 } from '../ui/stepper'
 import { StepHeader } from './step-header'
 
+const personalDataStepSchema = z.object({
+  firstName: z.string().min(1, 'Informe o seu primeiro nome'),
+  lastName: z.string().min(1, 'Informe o seu último nome'),
+  document: z.string().min(1, 'Informe o seu CPF'),
+})
+
 export function PersonalDataStep() {
+  const { nextStep } = useStepper()
+
+  const form = useForm({
+    resolver: zodResolver(personalDataStepSchema),
+    defaultValues: {
+      document: '',
+    },
+  })
+
+  const handleSubmit = form.handleSubmit(async (formData) => {
+    console.log(formData)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    nextStep()
+  })
+
   return (
-    <div className="w-full">
+    <form className="w-full" onSubmit={handleSubmit}>
       <StepHeader
         title="Dados pessoais"
         description="Conte-nos mais sobre você"
@@ -17,26 +48,80 @@ export function PersonalDataStep() {
 
       <FieldGroup>
         <Field>
-          <FieldLabel htmlFor="first_name">Primeiro nome</FieldLabel>
-          <Input id="first_name" />
+          <FieldLabel htmlFor="firstName">Primeiro nome</FieldLabel>
+
+          <Input id="firstName" {...form.register('firstName')} />
+
+          <ErrorMessage
+            errors={form.formState.errors}
+            name="firstName"
+            render={({ message }) => (
+              <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertDescription>{message}</AlertDescription>
+              </Alert>
+            )}
+          />
         </Field>
 
         <Field>
-          <FieldLabel htmlFor="last_name">Sobrenome</FieldLabel>
-          <Input id="last_name" />
+          <FieldLabel htmlFor="lastName">Sobrenome</FieldLabel>
+
+          <Input id="lastName" {...form.register('lastName')} />
+
+          <ErrorMessage
+            errors={form.formState.errors}
+            name="lastName"
+            render={({ message }) => (
+              <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertDescription>{message}</AlertDescription>
+              </Alert>
+            )}
+          />
         </Field>
 
         <Field>
           <FieldLabel htmlFor="document">CPF</FieldLabel>
-          <Input id="document" />
+
+          <Controller
+            control={form.control}
+            name="document"
+            render={({ field: { value, ref, onChange } }) => (
+              <PatternFormat
+                id="document"
+                format="###.###.###-##"
+                mask="_"
+                value={value}
+                getInputRef={ref}
+                onValueChange={(values) => onChange(values.value)}
+                customInput={Input}
+              />
+            )}
+          />
+
+          <ErrorMessage
+            errors={form.formState.errors}
+            name="document"
+            render={({ message }) => (
+              <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertDescription>{message}</AlertDescription>
+              </Alert>
+            )}
+          />
         </Field>
       </FieldGroup>
 
       <StepperFooter>
-        <StepperBackButton />
+        <StepperBackButton disabled={form.formState.isSubmitting} />
 
-        <StepperNextButton />
+        <StepperNextButton
+          type="submit"
+          onClick={handleSubmit}
+          disabled={form.formState.isSubmitting}
+        />
       </StepperFooter>
-    </div>
+    </form>
   )
 }
